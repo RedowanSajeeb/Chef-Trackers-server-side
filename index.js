e18b0b8f7b9931f0fce575329a55770d105930e7
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-05-02 11:54:42 +0600

    data-json-add

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000;
-
+const chef = require('./Data/chefs.json')
 app.get('/',(req,res)=>{
     res.send("Hello World!!");
 })
+app.get("/chef", (req, res) => {
+  res.send(chef);
+});
 
 
 app.listen(port,()=>{
