092f12be2e4f29137e4db788efeb7d5be44825c4
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-05-03 03:39:31 +0600

    cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 const chef = require('./Data/chefs.json')
+app.use(cors());
 app.get('/',(req,res)=>{
     res.send("Hello World!!");
 })
